875a75c6970e0ef1765fc0b52d932cc2a5460549
Ignatius Bagus <ignatiusmbs@gmail.com>
2020-02-14 02:30:32 +0700

    Add gitconfig to private folder

--- install/symlink.sh
+++ install/symlink.sh
@@ -1,18 +1,25 @@
 #!/bin/bash
 
-DOTFILES=(
-  "bashrc"
-  "zshrc"
-  "zsh-filetype-highlighting"
-  "p10k.zsh"
+# shared dotfiles
+PB_DFS=(
+  ".bashrc"
+  ".zshrc"
+  ".zsh-filetype-highlighting"
+  ".p10k.zsh"
 )
 
-echo -e "\n>---------- Symbolic Links ----------<\n"
+# private dotfiles
+PV_DFS=(
+  ".gitconfig"
+)
 
 # Delete existing dot files and folders
-for DF in "${DOTFILES[@]}"; do
-  sudo rm -rf ~/.$DF >/dev/null 2>&1
-  ln -sfv "$(pwd)/shared/$DF" ~/.$DF
+for DF in "${PB_DFS[@]}"; do
+  ln -sfv "$(pwd)/shared/$DF" ~/$DF
+done
+echo
+for DF in ${PV_DFS[@]}; do
+  ln -sfv "$(pwd)/private/$DF" ~/$DF
 done
 
-echo -e "\nhttps://github.com/ignatiusmb/dotfiles\n"
+echo -e "\nhttps://github.com/ignatiusmb/dotfiles"
